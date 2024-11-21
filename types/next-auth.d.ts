62dccec2f3cf3fce63d9c7b5e966ad83c11d054b
nicolasfrouin-dgrp <nicolas.f@dental-groupe.com>
2024-11-21 10:34:07 +0100

    initial commit

---
+++ types/next-auth.d.ts
@@ -0,0 +1,24 @@
+/*
+import { DefaultSession } from 'next-auth';
+
+// Typescript will merge these modules with the existing NextAuth types
+// to add the `api_key` field to the user object
+
+declare module 'next-auth' {
+  interface Session {
+    user: {
+      api_key?: string;
+    } & DefaultSession['user'];
+  }
+  interface User {
+    api_key?: string;
+  }
+}
+
+declare module 'next-auth/jwt' {
+  // Returned by the `jwt` callback and `useSession`, when using JWT sessions
+  interface JWT {
+    api_key?: string;
+  }
+}
+*/
